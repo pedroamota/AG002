4a27ce4d73b5209475a1b3a2eb5fd5ab021fe70d
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-06-11 05:09:33 -0300

    :sparkles: Feat: Questão, convertendo valores

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import pandas as pd
 data = pd.read_csv('./penguins.csv')
 
 def convertValues(data):
-    # Mapeamento dos valores
-    mapeamento_island = {'Biscoe': 0, 'Dream': 1, 'Torgersen': 2}
-    mapeamento_sex = {'FEMALE': 0, 'MALE': 1}
-    mapeamento_species = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
+    # Map dos valores
+    map_island = {'Biscoe': 0, 'Dream': 1, 'Torgersen': 2}
+    map_sex = {'FEMALE': 0, 'MALE': 1}
+    map_species = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
 
-    # Aplicando o mapeamento
-    data['island'] = data['island'].map(mapeamento_island)
-    data['sex'] = data['sex'].map(mapeamento_sex)
-    data['species'] = data['species'].map(mapeamento_species)
+    # Aplicando o map
+    data['island'] = data['island'].map(map_island)
+    data['sex'] = data['sex'].map(map_sex)
+    data['species'] = data['species'].map(map_species)
 
     return data
 
