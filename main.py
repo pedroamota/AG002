09a40ca95ff60f25c8667238bb76b7af9b096ffa
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-06-11 05:08:17 -0300

    :sparkles: Feat: Questão, convertendo valores

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import pandas as pd
 
 data = pd.read_csv('./penguins.csv')
 
+def convertValues(data):
+    # Mapeamento dos valores
+    mapeamento_island = {'Biscoe': 0, 'Dream': 1, 'Torgersen': 2}
+    mapeamento_sex = {'FEMALE': 0, 'MALE': 1}
+    mapeamento_species = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
+
+    # Aplicando o mapeamento
+    data['island'] = data['island'].map(mapeamento_island)
+    data['sex'] = data['sex'].map(mapeamento_sex)
+    data['species'] = data['species'].map(mapeamento_species)
+
+    return data
+
+data = convertValues(data)
+
+print(data.head())
